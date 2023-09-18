5d9ab52449bb89f2dfd4460fc7b4b4604444b27b
Oleg <olegburas49@gmail.com>
2023-09-19 00:06:23 +0300

    end

--- lab1.c
+++ lab1.c
@@ -4,20 +4,18 @@
 #include <stdio.h>
 #include <math.h>
 
-double calculateExpression(double x, double y, double z) {
-    double result = (3 * pow(x, 2) + 1) / (5 * y) + (2 * tan(x + 1)) / (y - 2);
+double calculateExpression(double x, double y) {
+    double result = 3 * pow(x, 2) + 1 + 2 * tan(x + 1) + 5 * y * (y - 2);
     return result;
 }
 
 int main() {
-    double x, y, z;
-    printf("Enter the values for x, y, and z: ");
-    scanf("%lf %lf %lf", &x, &y, &z);
+    double x, y;
+    printf("Enter the values for x, y:");
+    scanf("%lf %lf %lf", &x, &y);
 
-    double result = calculateExpression(x, y, z);
+    double result = calculateExpression(x, y);
     printf("Result: %.2lf", result);
 
     return 0;
 }
-
-
